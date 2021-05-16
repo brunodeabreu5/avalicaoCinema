5bfe0591f0300a0981a1d4ffcaf53e86a1062b35
Bruno <brunodornelesdeabreu@hotmail.com>
2021-05-16 11:34:54 -0300

    nova brach

--- casadooleo_backend/src/main/java/com/example/casadooleo_backend/controller/CliennteResource.java
+++
@@ -1,9 +0,0 @@
-package com.example.casadooleo_backend.controller;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
-public class CliennteResource {
-
-}
---
+++ casadooleobackend/src/main/java/com/backend/casadooleo/CasadooleoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.casadooleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadooleoApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(CasadooleoApplication.class, args);
+    }
+
+}
