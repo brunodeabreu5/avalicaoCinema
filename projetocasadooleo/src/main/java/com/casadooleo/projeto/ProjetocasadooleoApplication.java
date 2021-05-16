935ed92ac6b9f75b45b3dc19377fa03b5b3900a9
Bruno <brunodornelesdeabreu@hotmail.com>
2021-05-16 11:12:16 -0300

    nova branch

---
+++ projetocasadooleo/src/main/java/com/casadooleo/projeto/ProjetocasadooleoApplication.java
@@ -0,0 +1,13 @@
+package com.casadooleo.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocasadooleoApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(ProjetocasadooleoApplication.class, args);
+    }
+
+}
---
+++ projetocasadooleo/src/test/java/com/casadooleo/projeto/ProjetocasadooleoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casadooleo.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocasadooleoApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
